f8bab70712e3c0ddaf6d9294c7827f8f507b7afc
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-21 10:29:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * @author Pacifique LINJANJA
+ */
+
+const data = [
+
+];
+
+/**
+ * To calculate enthropia, we need :
+ * @ClasssName : the principal class which is going to allow us to take decision
+ * @data : the entire array of 
+ */
+
+const calculateEnthropia  = (className, data) => {
+
+}
+/**
+ * To calculate Gain, we need, the global enthropia, and the name of the field we need to evaluate
+ * Eventually the enthropia of the corresponding field
+ */
+const calculateGain = () => {
+
+}
